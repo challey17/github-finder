44de69e6755b4522bd25a2e56a3e6257ecd3634d
Chayce Knaub <chknaub@yahoo.com>
2020-10-11 22:27:03 -0400

    component level state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, {Component} from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-
-class App extends React.Component {
+import UserItem from "./components/users/UserItem"
+class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <UserItem />
       </div>
     );
   }
