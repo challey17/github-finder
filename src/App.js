0ed314a93708db76220e6b82d1852498c779c053
Chayce Knaub <chknaub@yahoo.com>
2020-10-11 20:07:56 -0400

    initial navbar set up, chrome warnings about dev tools

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
 
 class App extends React.Component {
   render() {
-    const name = "John Doe";
-    const foo = () => "BAR";
     return (
       <div className="App">
-        <h1>Hello {foo()}</h1>
+        <Navbar />
       </div>
     );
   }
