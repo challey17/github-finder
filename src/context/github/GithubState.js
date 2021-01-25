36904c66eef96cc6081783a3f4dd1b82564dfeb0
Chayce Knaub <chknaub@yahoo.com>
2021-01-24 21:29:44 -0500

    started implementing context

---
+++ src/context/github/GithubState.js
@@ -0,0 +1,49 @@
+import React, { useReducer } from "react";
+import axios from "axios";
+import githubContext from "./githubContext";
+import GithubReducer from "./githubReducer";
+import {
+  SEARCH_USERS,
+  SET_LOADING,
+  CLEAR_USERS,
+  GET_REPOS,
+  GET_USER,
+} from "../types";
+
+// set initial state and actions/methods
+
+const GithubState = (props) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+  // Search Users
+
+  //Get User
+
+  //Get Repos
+
+  //Clear Users
+
+  //Set Laoding
+
+  return (
+    <githubContext.Provider
+      value={{
+        users: state.user,
+        user: state.user,
+        repos: state.repos,
+        loading: state.loading,
+      }}
+    >
+      {props.children}
+    </githubContext.Provider>
+  );
+};
+
+export default GithubState;
---
+++ src/context/github/githubContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+//this file just initializes a new context
+const githubContext = createContext();
+
+export default githubContext;
